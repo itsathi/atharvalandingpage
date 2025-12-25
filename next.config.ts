9d661842db64d06093f5d6bbe6796cd6e1a30271
itsathi <atharvaacharya026@gmail.com>
2025-12-25 12:08:02 +0530

    Ignore lint and TS errors during build

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
