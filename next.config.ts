9a0a872f0846f6c1739e15d0af591c80568bfe3f
itsathi <atharvaacharya026@gmail.com>
2025-12-25 12:02:15 +0530

    Fix build errors and config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-
   eslint: {
     ignoreDuringBuilds: true,
   },
-
   typescript: {
     ignoreBuildErrors: true,
   },
